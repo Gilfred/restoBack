aeb0df3b65401d9159c68687264ce3c15fa5ce1f
Gilfred Dgb <gilfredmawulomdgb@gmail.com>
2026-08-07 00:28:29 +0100

    mise a jour en prod

--- app/main.py
+++ app/main.py
@@ -41,9 +41,9 @@ app.add_middleware(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
-        # "https://grandresto.gilexist.workers.dev",
+        # "http://localhost:5173",
+        # "http://127.0.0.1:5173",
+        "https://grandresto.gilexist.workers.dev",
     ],
     allow_credentials=True,
     allow_methods=["*"],
