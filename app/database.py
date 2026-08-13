957cde412cd0bc6389a5d5d5c3439e0a3f7c4282
Gilfred Dgb <gilfredmawulomdgb@gmail.com>
2026-08-13 21:32:25 +0100

    retour a la version precedante

--- app/database.py
+++ app/database.py
@@ -12,22 +12,11 @@ if not DATABASE_URL:
         "Please check your .env file or environment variables."
     )
 
-engine = create_engine(
-    DATABASE_URL,
-    pool_pre_ping=True,
-    pool_recycle=1800,
-)
-
-SessionLocal = sessionmaker(
-    autocommit=False,
-    autoflush=False,
-    bind=engine,
-)
-
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 def create_db_and_tables():
     Base.metadata.create_all(engine)
-
 
 def get_session():
     db = SessionLocal()
